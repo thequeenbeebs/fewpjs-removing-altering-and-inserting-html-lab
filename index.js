6b970cda5f81f16f5070e63174bf578f2781b254
Blaire Baker <bakerblaire@gmail.com>
2021-01-09 15:41:01 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+let element = document.querySelector("main");
+
+element.remove();
+
+let newHeader;
+
+let victory = document.createElement('h1');
+victory.id = "victory";
+
+document.body.appendChild(victory);
+
+newHeader = document.querySelector('h1#victory');
+
+newHeader.innerHTML = "is the champion";
+
+
+
